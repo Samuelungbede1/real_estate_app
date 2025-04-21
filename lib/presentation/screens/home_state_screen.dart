e5b5285f47c2510a5ae1ef8612c3f9b5e11f5f64
Onum Samuel <samuel.onum1@gmail.com>
2025-04-21 01:50:08 +0100

    added heart beat effect to bottom nav onclick item

--- lib/presentation/screens/home_state_screen.dart
+++ lib/presentation/screens/home_state_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:real_estate_app/core/utils/app_colors.dart';
-import 'package:real_estate_app/core/utils/assets.dart';
 import 'package:real_estate_app/core/utils/dashboard_animation.dart';
 import 'package:real_estate_app/presentation/screens/dasboard_screen.dart';
 import 'package:real_estate_app/presentation/screens/map_screen.dart';
-import 'package:real_estate_app/presentation/widgets/custom_icon.dart';
+
+import '../widgets/ripple_animation.dart';
 
 class HomeScreenState extends StatefulWidget {
   const HomeScreenState({super.key});
@@ -22,7 +21,7 @@ class _HomeScreenStateState extends State<HomeScreenState>
   @override
   void initState() {
     super.initState();
-    animations = DashboardAnimations(this); // 'this' is the TickerProvider
+    animations = DashboardAnimations(this);
   }
 
   @override
@@ -55,8 +54,8 @@ class _HomeScreenStateState extends State<HomeScreenState>
           builder: (context, child) {
             return Positioned(
               bottom: animations.bottomNavPosition.value,
-              left: 80,
-              right: 80,
+              left: 70,
+              right: 70,
               child: child!,
             );
           },
@@ -69,7 +68,7 @@ class _HomeScreenStateState extends State<HomeScreenState>
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                _buildNavItem(0, Icons.search_rounded, _selectedIndex == 0),
+                _buildNavItem(0, Icons.circle, _selectedIndex == 0),
                 _buildNavItem(1, Icons.messenger_outlined, _selectedIndex == 1),
                 _buildNavItem(2, Icons.home_filled, _selectedIndex == 2),
                 _buildNavItem(3, Icons.favorite, _selectedIndex == 3),
@@ -82,28 +81,130 @@ class _HomeScreenStateState extends State<HomeScreenState>
     );
   }
 
-  Widget _buildNavItem(int index, IconData icon, bool isSelected) {
-    return GestureDetector(
-      onTap: () {
-        // _selectedIndex = index;
+  // Widget _buildNavItem(int index, IconData icon, bool isSelected) {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       setState(() {
+  //         _selectedIndex = index;
+  //       });
+  //     },
+  //     child: isSelected ?Container(
+  //       width: 45,
+  //       height: 45,
+  //       decoration: const BoxDecoration(
+  //         color:  Colors.orange,
+  //         shape: BoxShape.circle,
+  //       ),
+  //       child: Icon(
+  //         icon,
+  //         color: isSelected ? Colors.white : Colors.white,
+  //         size: 20,
+  //       ),
+  //     ): Container(
+  //       width: 38,
+  //       height: 38,
+  //       decoration: const BoxDecoration(
+  //         color:  Colors.black26,
+  //         shape: BoxShape.circle,
+  //       ),
+  //       child: Icon(
+  //         icon,
+  //         color: isSelected ? Colors.white : Colors.white,
+  //         size: 20,
+  //       ),
+  //     ),
+  //   );
+  // }
+// Widget _buildNavItem(int index, IconData icon, bool isSelected) {
+//   return Material(
+//     color: Colors.transparent,
+//     child: Stack(
+//       alignment: Alignment.center,
+//       children: [
+//         // Background container
+//         Container(
+//           width: isSelected ? 45 : 38,
+//           height: isSelected ? 45 : 38,
+//           decoration: BoxDecoration(
+//             color: isSelected ? Colors.orange : Colors.black26,
+//             shape: BoxShape.circle,
+//           ),
+//         ),
+//         // Icon
+//         Icon(
+//           icon,
+//           color: Colors.white,
+//           size: 20,
+//         ),
+//         // Ripple effect
+//         Positioned.fill(
+//           child: Material(
+//             color: Colors.transparent,
+//             child: InkWell(
+//               onTap: () {
+//                 setState(() {
+//                   _selectedIndex = index;
+//                 });
+//               },
+//               borderRadius: BorderRadius.circular(30),
+//               splashColor: Colors.white.withOpacity(0.5),
+//               highlightColor: Colors.white.withOpacity(0.2),
+//               radius: 40,
+//             ),
+//           ),
+//         ),
+       
+//       ],
+//     ),
+//   );
+// }
 
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        width: 38,
-        height: 38,
-        decoration: BoxDecoration(
-          color: isSelected ? Colors.orange : Colors.transparent,
-          shape: BoxShape.circle,
+
+Widget _buildNavItem(int index, IconData icon, bool isSelected) {
+  return RippleAnimation(
+    // size: isSelected ? 45 : 38,
+    onTap: () => setState(() => _selectedIndex = index),
+    child:  Material(
+    color: Colors.transparent,
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        // Background container
+        Container(
+          width: isSelected ? 45 : 38,
+          height: isSelected ? 45 : 38,
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.orange : Colors.black26,
+            shape: BoxShape.circle,
+          ),
         ),
-        child: Icon(
+        // Icon
+        Icon(
           icon,
-          color: isSelected ? Colors.white : Colors.white,
-          size: 16,
+          color: Colors.white,
+          size: 20,
         ),
-      ),
-    );
-  }
+        // // Ripple effect
+        // Positioned.fill(
+        //   child: Material(
+        //     color: Colors.transparent,
+        //     child: InkWell(
+        //       onTap: () {
+        //         setState(() {
+        //           _selectedIndex = index;
+        //         });
+        //       },
+        //       borderRadius: BorderRadius.circular(30),
+        //       splashColor: Colors.white.withOpacity(0.5),
+        //       highlightColor: Colors.white.withOpacity(0.2),
+        //       radius: 40,
+        //     ),
+        //   ),
+        // ),
+       
+      ],
+    ),
+  )
+  );
+}
 }
---
+++ lib/presentation/widgets/ripple_animation.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class RippleAnimation extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final double size;
+
+  const RippleAnimation({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.size = 50,
+  });
+
+  @override
+  State<RippleAnimation> createState() => _RippleAnimationState();
+}
+
+class _RippleAnimationState extends State<RippleAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _innerRadius;
+  late Animation<double> _outerRadius;
+  late Animation<double> _opacity;
+  late Animation<double> _innerStroke;
+  late Animation<double> _outerStroke;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _innerRadius = Tween<double>(begin: 3, end: widget.size * 0.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+      ),
+    );
+
+    _outerRadius =
+        Tween<double>(begin: widget.size * 0.15, end: widget.size * 0.45)
+            .animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.2, 0.7, curve: Curves.easeOut),
+      ),
+    );
+
+    _opacity = Tween<double>(begin: 0.6, end: 0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    // Stroke width animations
+    _innerStroke = Tween<double>(begin: 0, end: 6).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.3, curve: Curves.easeOut),
+      ),
+    );
+
+    _outerStroke = Tween<double>(begin: 0, end: 10).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.1, 0.4, curve: Curves.easeOut),
+      ),
+    );
+  }
+
+  void _handleTap() {
+    _controller.forward(from: 0).then((_) {
+      _controller.reverse();
+      widget.onTap?.call();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return CustomPaint(
+                size: Size(widget.size, widget.size),
+                painter: _RipplePainter(
+                  innerRadius: _innerRadius.value,
+                  outerRadius: _outerRadius.value,
+                  opacity: _opacity.value,
+                  innerStroke: _innerStroke.value,
+                  outerStroke: _outerStroke.value,
+                ),
+              );
+            },
+          ),
+          widget.child,
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class _RipplePainter extends CustomPainter {
+  final double innerRadius;
+  final double outerRadius;
+  final double opacity;
+  final double innerStroke;
+  final double outerStroke;
+
+  _RipplePainter({
+    required this.innerRadius,
+    required this.outerRadius,
+    required this.opacity,
+    required this.innerStroke,
+    required this.outerStroke,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+
+    // Outer circle
+    final outerPaint = Paint()
+      ..color = Colors.white.withOpacity(opacity)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = outerStroke;
+
+    // Inner circle
+    final innerPaint = Paint()
+      ..color = Colors.white.withOpacity(opacity * 0.8)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = innerStroke;
+
+    canvas.drawCircle(center, outerRadius, outerPaint);
+    canvas.drawCircle(center, innerRadius, innerPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant _RipplePainter oldDelegate) {
+    return innerRadius != oldDelegate.innerRadius ||
+        outerRadius != oldDelegate.outerRadius ||
+        opacity != oldDelegate.opacity ||
+        innerStroke != oldDelegate.innerStroke ||
+        outerStroke != oldDelegate.outerStroke;
+  }
+}
